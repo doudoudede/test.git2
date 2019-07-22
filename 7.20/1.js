85fab8845b48c85ff7ea44931862e2e2657b28e6
doudou <1455634164@qq.com>
2019-07-22 16:07:06 +0800

    nihao

--- 7.20/1.js
+++ 7.20/1.js
@@ -1,1 +1,10 @@
 // var express=require('http')
+【7.22】
+1.关系型数据库  特点:1.数据严格  比如:orancle  mysql sqllite sqlserve
+库与表的关系
+库 db   集合  table
+2.非关系型数据库    比如:mongodb  nosql
+数据库的作用：1.仓库
+            2.存取数据
+            3.面向文档    json
+库 db   集合 collection
